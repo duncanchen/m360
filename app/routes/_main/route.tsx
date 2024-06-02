4f4059e101be5461abb7a0e93df62e92b4a92665
Duncan Chen <duncanchen@gmail.com>
2024-06-02 09:42:38 -0500

    exteand the title

--- app/routes/_main/route.tsx
+++ app/routes/_main/route.tsx
@@ -1,27 +1,26 @@
-import { Outlet, json, useLoaderData, useNavigate } from "@remix-run/react"
-import { randomUser } from "~/lib/fake/name"
-import { useUserStore } from "~/components/store";
-import { useEffect } from "react";
+import { Outlet, useNavigate } from "@remix-run/react"
+import { useUserStore } from "~/components/store"
+import { useEffect } from "react"
 import { Button } from "~/components/ui/button"
 
 const readCookie = (name: string) => {
-  const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
-  if (match) return match[2];
+  const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"))
+  if (match) return match[2]
 }
 
 export default function Index() {
   const nav = useNavigate()
   useEffect(() => {
-    const user = readCookie("mt360_user");
+    const user = readCookie("mt360_user")
     if (user) {
-      useUserStore.getState().setUser(JSON.parse(atob(user)));
+      useUserStore.getState().setUser(JSON.parse(atob(user)))
     }
   }, [])
   return (
     <div className="dark bg-gray-800 h-screen text-foreground">
       <nav className="sticky top-0 bg-gray-900 p-2 ">
         <div className="container mx-auto flex flex-row items-center justify-between">
-          <div className="blue-gradient text-[2rem] font-mono font-bold italic">
+          <div className="blue-gradient text-[2rem] font-mono font-bold italic tracking-wider">
             {"mT360 "}
           </div>
           <Button variant="outline" onClick={() => nav("/logout")}>
@@ -33,5 +32,5 @@ export default function Index() {
         <Outlet />
       </div>
     </div>
-  );
+  )
 }
